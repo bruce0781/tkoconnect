ccb204d27885130875f0af3e2eecf797aa187dc9
Bruce Tian <65952137+bruce0781@users.noreply.github.com>
2024-08-26 21:45:22 +0800

    Create SSTAP.js

---
+++ provider/SSTAP.js
@@ -0,0 +1,6 @@
+module.exports = {
+  type: 'clash', // 订阅类型
+  url: 'https://sub.govpro.dev/114514?token=f6ac715f55910a1e03d48e95a1e950a8', // 订阅链接
+  udpRelay: true, // 添加UDP:true
+  addFlag: true   // 添加国旗
+};

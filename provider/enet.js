774b8db23da54b966336f20c7eaca292f02c612c
Bruce Tian <65952137+bruce0781@users.noreply.github.com>
2024-08-26 21:44:30 +0800

    Create enet.js

---
+++ provider/enet.js
@@ -0,0 +1,6 @@
+module.exports = {
+  type: 'clash', // 订阅类型
+  url: 'https://159.75.130.241/easynet/api/v1/client/subscribe?token=2324c48607e4bd61083814a7b873860a', // 订阅链接
+  udpRelay: true, // 添加UDP:true
+  addFlag: true   // 添加国旗
+};
